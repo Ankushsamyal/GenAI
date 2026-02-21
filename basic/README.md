06d5ad2e758bc3e251cad9f49db478e90b70ff58
ankush_samyal-admin <ankush.samyal@qtsolv.com>
2026-02-21 18:12:00 +0530

    Install Groq in local

--- basic/README.md
+++ basic/README.md
@@ -4,7 +4,14 @@ Here we have use console.groq.com api insted of open ai
 notes:-
 
 OpenAI api:-
- Text - Text to Text - GPT4 ,GPT_3.5
+ Text - Text to Text - GPT4 ,GPT_3.5 ,
+ Image - Text to Image - DALL-E,
+ Audio - Text to Speech , Speech to text - Whisper
+ Sora - Text to Video
+
+
+ Groq api:-
+ Text - Text to Text - llama-3.3-70b-versatile,
  Image - Text to Image - DALL-E,
  Audio - Text to Speech , Speech to text - Whisper
  Sora - Text to Video
--- basic/src/index.js
+++ basic/src/index.js
@@ -1,19 +1,16 @@
-import {OpenAI} from 'openai'
+import Groq from 'groq-sdk'
 
-const openai = new OpenAI({
-  apiKey: process.env.GROQ_API_KEY,
-  baseURL: "https://api.groq.com/openai/v1",
-})
-async function main(){
-    const response = await openai.chat.completions.create({
-        model:'llama-3.3-70b-versatile',
-        messages:[{
+const groq = new Groq()
+
+async function main() {
+    const response = await groq.chat.completions.create({
+        model: 'llama-3.3-70b-versatile',
+        messages: [{
             role: 'user',
-            content:'How tall is mount status of iron man of india'
+            content: 'How tall is mount status of iron man of india'
         }]
     })
     console.log(response.choices[0].message.content)
-
 }
 
 main();

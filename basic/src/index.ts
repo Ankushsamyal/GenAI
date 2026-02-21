9e5da5ea20c4d0240c6ba2e12d8628dad0a8ebd9
ankush_samyal-admin <ankush.samyal@qtsolv.com>
2026-02-22 00:49:25 +0530

    how to change style

--- basic/src/index.ts
+++ basic/src/index.ts
@@ -1,16 +1,30 @@
-import Groq from 'groq-sdk'
+import Groq from "groq-sdk";
+import { encoding_for_model } from "tiktoken";
 
-const groq = new Groq()
+const groq = new Groq();
 
 async function main() {
     const response = await groq.chat.completions.create({
-        model: 'llama-3.3-70b-versatile',
-        messages: [{
-            role: 'user',
-            content: 'How tall is mount status of iron man of india'
-        }]
-    })
-    console.log(response.choices[0].message.content)
+        model: "llama-3.3-70b-versatile",
+        messages: [
+            {
+                role: "system",
+                content: "you response like a bro ",
+            },
+            {
+                role: "user",
+                content: "How tall is mount status of iron man of india",
+            },
+        ],
+    });
+    console.log(response.choices[0].message);
+}
+
+function encodePrompt() {
+    const prompt = "How tall is mount status of iron man of india";
+    const encoder = encoding_for_model("chatgpt-4o-latest");
+    const words = encoder.encode(prompt);
+    console.log(words);
 }
 
 main();

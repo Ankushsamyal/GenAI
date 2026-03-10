9962522b5604607c7260ad6e1da000a8bc852936
ankush_samyal-admin <ankush.samyal@qtsolv.com>
2026-03-10 19:15:14 +0530

    implementing AI tools or function for real time contest

---
+++ AI_tools(function)/src/index.ts
@@ -0,0 +1,24 @@
+import Groq from "groq-sdk";
+
+const groqAI = new Groq();
+console.log('first')
+
+async function callGroqAIWithTools() {
+    const context: Groq.Chat.ChatCompletionMessageParam[] = [
+        {
+            role: 'system',
+            content: 'you are a helpfull ai assistant'
+        },
+        {
+            role: 'user',
+            content: 'what is the time of day'
+
+        }]
+    const response = await groqAI.chat.completions.create({
+        model: 'llama-3.3-70b-versatile',
+        messages: context
+    })
+    console.log(response.choices[0].message.content)
+}
+
+callGroqAIWithTools();

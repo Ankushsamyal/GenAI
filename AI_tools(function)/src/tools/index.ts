84a02535bc6f52b80481bd8d037f2aee121e197b
ankush_samyal-admin <ankush.samyal@qtsolv.com>
2026-04-01 14:56:30 +0530

    how to pass argument in function

--- AI_tools(function)/src/tools/index.ts
+++ AI_tools(function)/src/tools/index.ts
@@ -1,4 +1,5 @@
 import Groq from "groq-sdk";
+import { describe } from "node:test";
 
 const groqAI = new Groq();
 
@@ -8,15 +9,25 @@ function getTimeOfDay() {
     return timeString
 }
 
+function getOrderStatus(orderId: string) {
+    console.log(`Getting the status of order ${orderId}`)
+    const orderAsNumber = parseInt(orderId);
+    if (orderAsNumber % 2 == 0) {
+        return 'IN_PROGRESS'
+    }
+    return 'COMPLETED'
+}
+
 async function callGroqAIWithTools() {
     const context: Groq.Chat.ChatCompletionMessageParam[] = [
         {
             role: 'system',
-            content: 'you are a helpfull, friendly like childhood friend ans with some shit words '
+            content: 'you are a helpfull assistant that gives information about the time of day and order status'
         },
         {
             role: 'user',
-            content: 'what is the time of day'
+            // content: 'what is the time of day'
+            content: 'what is the status of order 1235?'
 
         }]
     const response = await groqAI.chat.completions.create({
@@ -30,6 +41,23 @@ async function callGroqAIWithTools() {
                     description: 'get the time of day'
                 }
 
+            },
+            {
+                type: 'function',
+                function: {
+                    name: 'getOrderStatus',
+                    description: ' Return the status of an order',
+                    parameters: {
+                        type: 'object',
+                        properties: {
+                            orderId: {
+                                type: 'string',
+                                describe: 'The id of the order to get the status of'
+                            }
+                        },
+                        require: ['orderId']
+                    }
+                }
             }
         ],
         tool_choice: 'auto' // the engine will decide which tool to use
@@ -42,6 +70,18 @@ async function callGroqAIWithTools() {
         const toolName = toolCall.function.name;
         if (toolName == 'getTimeOfDay') {
             const toolResponse = getTimeOfDay()
+
+            context.push(response.choices[0].message);
+            context.push({
+                role: 'tool',
+                content: toolResponse,
+                tool_call_id: toolCall.id
+            })
+        }
+        if (toolName == 'getOrderStatus') {
+            const rawArgument = toolCall.function.arguments;
+            const parsedArgument = JSON.parse(rawArgument);
+            const toolResponse = getOrderStatus(parsedArgument.orderId);
 
             context.push(response.choices[0].message);
             context.push({
